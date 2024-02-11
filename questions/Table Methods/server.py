598eed52ebcbdba6b13e9a5294987cb5a5c4ac38
Sabrina <sabrinakma@berkeley.edu>
2024-02-10 17:21:02 -0800

    table methods --> changing tables?

--- questions/Table Methods/server.py
+++ questions/Table Methods/server.py
@@ -4,7 +4,7 @@ import pandas as pd
 import prairielearn as pl
 
 
-def generate(data):
+def generate(data):    
     d = {
     "Actor": ["Harrison Ford", "Samuel L. Jackson", "Morgan Freeman", "Tom Hanks", "Robert Downey Jr.", "Eddie Murphy"],
     "Total Gross": [4871.7, 4772.8, 4468.3, 4340.8, 3947.3, 3810.4],
@@ -14,14 +14,35 @@ def generate(data):
     "Gross": [936.7,623.4,534.9,415,623.4,441.2]
         }
     df = pd.DataFrame(data=d)
-    df = df.round(2)
-    df.reset_index(drop=True, inplace=True)
-    
-    df_sorted = df.sort_values(by="Number of Movies", ascending=False)
+    #df_sorted = df.sort_values(by="Number of Movies", ascending=False)
 
+    data["params"]["df"] = pl.to_json(df)
+    data["correct_answers"]["mcq"] = "SORT"
+    data["correct_answers"]["desc"] = "TRUE"
     
-    data["params"]["df"] = pl.to_json(df_sorted)
-
+def parse(data):
+    # use get() for submitted_answers in case no answer was submitted
+    # get the submitted answer, defaulting to empty string if it's missing
+    mcq = data["submitted_answers"].get("mcq", "")
+    desc = data["submitted_answers"].get("desc","")
+    
+    d = {
+        "Actor": ["Harrison Ford", "Samuel L. Jackson", "Morgan Freeman", "Tom Hanks", "Robert Downey Jr.", "Eddie Murphy"],
+        "Total Gross": [4871.7, 4772.8, 4468.3, 4340.8, 3947.3, 3810.4],
+        "Number of Movies": [41,69,61,44,53,38],
+        "Avg per Movie": [118.8,69.2,73.3,98.7,74.5,100.3],
+        "#1 Movie": ["Star Wars: The Force Awakens", "The Avengers", "The Dark Knight", "Toy Story 3", "The Avengers", "Shrek 2"],
+        "Gross": [936.7,623.4,534.9,415,623.4,441.2]
+        }
+    df = pd.DataFrame(data=d)
+    
+    if mcq == "SORT" and desc == "TRUE":
+        df_sorted = df.sort_values(by="Number of Movies", ascending=False)
+        #data["params"]["df_correct"] = pl.to_json(df_sorted) --> errors!! can't modify params???
+    elif mcq == "SORT" and desc == "FALSE":
+        df_sorted = df.sort_values(by="Number of Movies", ascending=True)
+        #data["params"]["df_incorrect"] = pl.to_json(df_sorted)
+        
 # def generate(data):
     # # Generates number of events.
     # x_events = random.randint(4, 7)
