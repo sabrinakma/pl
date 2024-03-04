c6ce8394d6bde7c573169e1ca21c8a9191a5a3f0
Sabrina <sabrinakma@berkeley.edu>
2024-03-04 00:55:17 -0800

    visualize

--- questions/Table1.5/server.py
+++ questions/Table1.5/server.py
@@ -1,14 +1,15 @@
 import random
-
 import pandas as pd
 import prairielearn as pl
+
+from flask import Flask, request, jsonify
 
 #import d8 package?
 # from datascience import *
 import datascience as ds
 
+
 def generate(data):
-    
     data["correct_answers"]["mcq"] = "SORT"
     data["correct_answers"]["desc"] = "TRUE"
     
@@ -39,7 +40,31 @@ def generate(data):
     #     else:    
     #         data["params"]["df"] = pl.to_json(df)
     # else:
-    #     data["params"]["df"] = pl.to_json(df)   
+    #     data["params"]["df"] = pl.to_json(df)  
+
+def generate_2(data, mcq, desc):
+    # Based on the selected options for mcq and desc, generate the DataFrame
+    
+    d = {
+        "Actor": ["Harrison Ford", "Samuel L. Jackson", "Morgan Freeman", "Tom Hanks", "Robert Downey Jr.", "Eddie Murphy"],
+        "Total Gross": [4871.7, 4772.8, 4468.3, 4340.8, 3947.3, 3810.4],
+        "Number of Movies": [41,69,61,44,53,38],
+        "Avg per Movie": [118.8,69.2,73.3,98.7,74.5,100.3],
+        "#1 Movie": ["Star Wars: The Force Awakens", "The Avengers", "The Dark Knight", "Toy Story 3", "The Avengers", "Shrek 2"],
+        "Gross": [936.7,623.4,534.9,415,623.4,441.2]
+    }
+    
+    df = pd.DataFrame(data=d)
+    
+    # Apply operations based on mcq and desc
+    if mcq == "SORT":
+        if desc == "TRUE":
+            df_sorted = df.sort_values(by="Number of Movies", ascending=False)
+            data["params"]["df"] = pl.to_json(df_sorted)
+        elif desc == "FALSE":
+            df_sorted = df.sort_values(by="Number of Movies", ascending=True)
+            data["params"]["df"] = pl.to_json(df_sorted)
+
     
 def parse(data):
     if data["submitted_answers"]["mcq"] == "":
@@ -106,4 +131,16 @@ def grade(data):
 #     elif mcq == "SORT" and desc == "FALSE":
 #         df_sorted = df.sort_values(by="Number of Movies", ascending=True)
 #         #data["params"]["df_incorrect"] = pl.to_json(df_sorted)
-        
+    
+root_path = '/Users/sabrinama/Downloads/pl'
+app = Flask(__name__, root_path = root_path)
+@app.route('/grade',methods=['POST'])
+def grade_question():
+    if request.method == 'POST':
+        mcq = request.json.get('mcq','')
+        desc = request.json.get('desc','')
+        generate_2(request.json,mcq,desc)
+        return jsonify({'message':'data is sent'})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
